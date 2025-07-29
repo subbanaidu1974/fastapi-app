d0337a26875b109e0550f182335ccda52409190e
subbanaidu1974 <subbaopenapi@gmail.com>
2025-07-28 23:33:47 -0400

    update 2

--- main.py
+++ main.py
@@ -4,5 +4,5 @@ app = FastAPI()
 
 @app.get("/")
 def read_root():
-        return {"message": "FastAPI is live!. start writing the apis this is to test the api"}
+        return {"message": "FastAPI is live! live! live!. start writing the apis this is to test the api"}
 
