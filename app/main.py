33a8cf00c9a76b8821f6b3d198b06f2811ab2567
subbanaidu1974 <subbaopenapi@gmail.com>
2025-12-29 23:13:07 -0500

    Update CORS origins to include port 8080

--- app/main.py
+++ app/main.py
@@ -18,7 +18,7 @@ import os
 
 origins = {
     "http://localhost:4200",
-    "http://localhost:4200",
+    "http://localhost:8080",
     "http://127.0.0.1:4200",
     "https://api.accessapis.com",
     "https://accessapis.com"
